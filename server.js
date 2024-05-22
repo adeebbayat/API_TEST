a1c38ab04103502edb2681532197acbb7d3dc61b
adeebbayat <adeebbayat@yahoo.com>
2024-05-22 15:37:59 -0700

    second commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req,res) => {
+  res.json({
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  });
+})
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
+
+module.exports = app;
