b280ceaf476669bcdaafe977a2745ed7425353a6
adeebbayat <adeebbayat@yahoo.com>
2024-05-29 15:38:30 -0700

    adeeb

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-
-app.get('/', (req,res) => {
+app.get("/", (req, res) => {
   res.json({
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
   });
-})
+});
 
+app.get("/adeeb", (req, res) => {
+  res.json({ firstName: "Adeeb", lastName: "Bayat" });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
+});
 
 module.exports = app;
